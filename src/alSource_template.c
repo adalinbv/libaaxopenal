d3f7bbf3733821ea18d2c7f40bb6febd29184f28
Erik Hofman <erik@ehofman.com>
2013-08-23 19:26:34 +0200

    explitic distance delay reinitialization by calling AL_SOURCE_STATE with AL_DISTANCE_DELAY_AAX

--- src/alSource_template.c
+++ src/alSource_template.c
@@ -140,6 +140,11 @@ ALSOURCE(N)(ALuint id, ALenum attrib, T value)
             if (ival == AL_PLAYING) alSourcePlayv(1, &id);
             else if (ival == AL_STOPPED) alSourceStopv(1, &id);
             else if (ival == AL_PAUSED) alSourcePausev(1, &id);
+            else if (ival == AL_DISTANCE_DELAY_AAX) {
+                 /* calling AAX_PLAYING on an already playig emitter */
+                 /* causes the distance delay to reinitialize.       */
+                 aaxEmitterSetState(src->handle, AAX_PLAYING);
+            }
             else _oalStateSetError(AL_INVALID_VALUE);
             break;
         case AL_SOURCE_RELATIVE:
